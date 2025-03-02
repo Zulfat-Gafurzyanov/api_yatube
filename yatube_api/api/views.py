c21b018d04884ae7b45e09d95a0ff7ba36c63420
Zulfat <gafurzyanov.zulfat@gmail.com>
2025-03-02 20:44:23 +1000

    Устранил ошибки.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.exceptions import PermissionDenied
 
@@ -7,7 +7,26 @@ from posts.models import Comment, Group, Post
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
-class CommentViewSet(viewsets.ModelViewSet):
+class UpdateDeleteViewSet(mixins.UpdateModelMixin,
+                          mixins.DestroyModelMixin,
+                          viewsets.GenericViewSet):
+    """Миксин для проверки прав при запросе на изменение или удаление."""
+
+    def perform_update(self, serializer):
+        # Функция проверяет, что только автор может вносить изменения.
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(UpdateDeleteViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        # Функция проверяет, что только автор может удалить созданный объект.
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        # Удаляем пост.
+        instance.delete()
+
+
+class CommentViewSet(viewsets.ModelViewSet, UpdateDeleteViewSet):
     """Вьюсет для модели Comment."""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -17,28 +36,13 @@ class CommentViewSet(viewsets.ModelViewSet):
         return get_object_or_404(Post, id=self.kwargs.get('post_id'))
 
     def get_queryset(self):
-        # Авторизованному пользователю показываем все комментарии поста.
-        if self.request.user:
-            post = self.get_post()
-            return post.comments.all()
+        post = self.get_post()
+        return post.comments.all()
 
     def perform_create(self, serializer):
         # При создании нового комментария автор автоматически определяется
         # на основе текущего аутентифицированного пользователя.
         serializer.save(author=self.request.user, post=self.get_post())
-
-    def perform_update(self, serializer):
-        # Функция проверяет, что только автор может изменить свой комментарий.
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого комментария запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        # Функция проверяет, что только автор может удалить свой комментарий.
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого комментария запрещено!')
-        # Удаляем пост.
-        instance.delete()
 
 
 class GroupViewSet(ReadOnlyModelViewSet):
@@ -47,7 +51,7 @@ class GroupViewSet(ReadOnlyModelViewSet):
     serializer_class = GroupSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet, UpdateDeleteViewSet):
     """Вьюсет для модели Post."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -56,19 +60,3 @@ class PostViewSet(viewsets.ModelViewSet):
         # При создании нового поста автор автоматически определяется на основе
         # текущего аутентифицированного пользователя.
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        # Функция проверяет, что только автор может изменить свой пост.
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого поста запрещено!')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        # Функция проверяет, что только автор может удалить свой пост.
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого поста запрещено!')
-        # Удаляем все связанные с постом комментарии.
-        for comment in instance.comments.all():
-            comment.delete()
-        # Удаляем пост.
-        instance.delete()
